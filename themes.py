e8ad1417b8967479014037948bacb1acca7c8e96
edr <github@rochefort.de>
2021-02-10 16:00:51 +0100

    Move themes to separate file. Proper solarized colors impl

---
+++ themes.py
@@ -0,0 +1,58 @@
+## SOLARIZED COLORS
+solarized_base03 = "#002b36"
+solarized_base02 = "#073642"
+solarized_base01 = "#586e75"
+solarized_base00 = "#657b83"
+solarized_base0 = "#839496"
+solarized_base1 = "#93a1a1"
+solarized_base2 = "#eee8d5"
+solarized_base3 = "#fdf6e3"
+solarized_yellow = "#b58900"
+solarized_orange = "#cb4b16"
+solarized_red = "#dc322f"
+solarized_magenta = "#d33682"
+solarized_violet = "#6c71c4"
+solarized_blue = "#268bd2"
+solarized_cyan = "#2aa198"
+solarized_green = "#859900"
+
+
+solarized_light = {
+    "background": solarized_base3,
+    "bodyText": solarized_base00,
+    "heading1": solarized_orange,
+    "headings": solarized_base01,
+    "links": solarized_orange,
+    "linksVisited": solarized_red,
+    "breadcrumb": solarized_base00,
+    "breadcrumbBackground": solarized_base2,
+    "code": solarized_base00,
+    "codeBackground": solarized_base2,
+    "nav": solarized_base01,
+    "navCurrent": solarized_base2,
+    "navHover": solarized_base01,
+    "navBackground": solarized_base2,
+    "navCurrentBackground": solarized_orange,
+    "navHoverBackground": solarized_base2,
+    "borders": solarized_base2,
+}
+
+solarized_dark = {
+    "background": solarized_base03,
+    "bodyText": solarized_base0,
+    "heading1": solarized_orange,
+    "headings": solarized_base1,
+    "links": solarized_orange,
+    "linksVisited": solarized_red,
+    "breadcrumb": solarized_base0,
+    "breadcrumbBackground": solarized_base02,
+    "code": solarized_base0,
+    "codeBackground": solarized_base02,
+    "nav": solarized_base1,
+    "navCurrent": solarized_base02,
+    "navHover": solarized_base1,
+    "navBackground": solarized_base02,
+    "navCurrentBackground": solarized_orange,
+    "navHoverBackground": solarized_base02,
+    "borders": solarized_base02,
+}
