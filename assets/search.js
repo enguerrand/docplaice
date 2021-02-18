b5547eb84d79882b560f397296228472a7666328
edr <github@rochefort.de>
2021-02-19 00:01:50 +0100

    Handle focus loss

--- assets/search.js
+++ assets/search.js
@@ -1,12 +1,14 @@
 const e = React.createElement;
 
-const EVENT_TYPES = ["change", "paste", "keyup"];
+const EVENT_TYPES = ["change", "paste", "keyup", "focus"];
 
 class SearchResults extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleFocusLoss = this.handleFocusLoss.bind(this);
         this.state = {};
+        this.monitorFocus = true;
         for (const key in search_index) {
             this.state[key] = false
         }
@@ -28,11 +30,26 @@ class SearchResults extends React.Component {
         console.log(search_string);
     }
 
+    handleFocusLoss(){
+        if (this.monitorFocus) {
+            this.setState((prevState, prevProps) => {
+                let nextState = {};
+                for (const key in search_index) {
+                    if (prevState[key]) {
+                        nextState[key] = false;
+                    }
+                }
+                return nextState;
+            });
+        }
+    }
+
     componentDidMount() {
         const element = document.getElementById("search");
         for (const t of EVENT_TYPES) {
             element.addEventListener(t, this.handleChange);
         }
+        element.addEventListener("blur", this.handleFocusLoss);
     }
 
     componentWillUnmount() {
@@ -40,6 +57,7 @@ class SearchResults extends React.Component {
         for (const t of EVENT_TYPES) {
             element.removeEventListener(t, this.handleChange);
         }
+        element.removeEventListener("blur", this.handleFocusLoss);
     }
 
     render() {
@@ -54,6 +72,7 @@ class SearchResults extends React.Component {
                             "a",
                             {
                                 key: key + "." + resultIndex,
+                                onMouseDown: () => {this.monitorFocus = false},
                                 href: match["url"]
                             },
                             match["name"]
@@ -62,9 +81,17 @@ class SearchResults extends React.Component {
                 }
             }
         }
+        let searchResultsClass;
+        if (entries.length > 0) {
+            searchResultsClass = "search-results-content";
+        } else {
+            searchResultsClass = "search-results-empty";
+        }
         return e(
             'div',
-            null,
+            {
+                className: searchResultsClass
+            },
             entries
         )
     }
